ac1bc870a950603bbe1c6965af3406def7a155af
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2023-07-20 17:53:57 +0800

    Added methods to exclude axioms different only by blank node ids from the list of changes while reloading ontologies from remote endpoints.

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/project/ProjectCache.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/project/ProjectCache.java
@@ -42,6 +42,8 @@ import edu.stanford.bmir.protege.web.shared.merge.OntologyDiff;
 import edu.stanford.bmir.protege.web.shared.project.*;
 import edu.stanford.bmir.protege.web.shared.revision.RevisionNumber;
 import edu.stanford.bmir.protege.web.shared.user.UserId;
+
+import java.text.Annotation;
 import java.text.Normalizer;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.query.BindingSet;
@@ -56,16 +58,15 @@ import org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf;
 import org.semanticweb.owlapi.formats.NTriplesDocumentFormat;
 import org.semanticweb.owlapi.io.OWLParserException;
 import org.semanticweb.owlapi.io.StringDocumentSource;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import java.io.IOException;
+import java.time.Clock;
+import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -239,8 +240,10 @@ public class ProjectCache implements HasDispose {
         ProjectId projectId = projectComponent.getProjectId();
         if (projectDetailsManager.isExistingProject(projectId)) {
             RevisionManager revisionManager = projectComponent.getRevisionManager();
+            RevisionNumber currentRevisionNo = revisionManager.getCurrentRevision();
+
             OWLOntologyManager owlOntologyManager = projectComponent.getRevisionManager()
-                .getOntologyManagerForRevision(revisionManager.getCurrentRevision());
+                .getOntologyManagerForRevision(currentRevisionNo );
             ProjectDetails projectDetails = projectDetailsManager.getProjectDetails(projectId);
             UserId userId = projectDetails.getOwner();
             IRI projectEndpoint = IRI.create(projectDetails.getProjectEndpoint());
@@ -288,11 +291,116 @@ public class ProjectCache implements HasDispose {
         ImmutableList.Builder<OntologyChange> changeList = ImmutableList.builder();
         for(OntologyDiff diff : ontologyDiffSet) {
             List<OntologyChange> changes = ontologyDiff2OntologyChanges.getOntologyChangesFromDiff(diff);
-            changeList.addAll(changes);
+            changeList.addAll(filterOutChangesDifferentOnlyByBlankNodeId(changes, ontologyB));
         }
         return changeList.build();
     }
 
+    private List<OntologyChange> filterOutChangesDifferentOnlyByBlankNodeId(List<OntologyChange> changes, OWLOntology ontology) {
+        Iterator<OntologyChange> it = changes.iterator();
+        Set<OntologyChange> changesToRemove = new HashSet<>();
+        Set<OWLAnnotation> annotationsAll = ontology.getAnnotations();
+        Set<OWLAxiom> axiomsAll = ontology.getAxioms();
+
+        while (it.hasNext()) {
+            OntologyChange change = it.next();
+            if (isChangeToRemove(change, annotationsAll, axiomsAll)) {
+                changesToRemove.add(change);
+            }
+        }
+
+        for (OntologyChange rmChange : changesToRemove) {
+            changes.remove(rmChange);
+        }
+
+        return changes;
+    }
+
+    private boolean isChangeToRemove(OntologyChange change, Set<OWLAnnotation> annotationsAll, Set<OWLAxiom> axiomsAll) {
+        if (change.isRemoveOntologyAnnotation() || change.isAddOntologyAnnotation()) {
+            if (isAnnotationChangeToRemove(change, annotationsAll)) {
+                return true;
+            }
+        } else if (change.isAxiomChange() || change.isRemoveAxiom() || change.isAddAxiom()) {
+            if (isAxiomChangeToRemove(change, axiomsAll)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isAnnotationChangeToRemove(OntologyChange change, Set<OWLAnnotation> annotationsAll) {
+        try {
+            OWLAnnotation annotation = change.getAnnotationOrThrow();
+            if (!annotation.getAnonymousIndividuals().isEmpty()) {
+                HashSet<OWLAnnotation> sameAn = getEqualAnnotationsExceptForBlankNodes(annotation, annotationsAll);
+                if (!sameAn.isEmpty()) {
+                    return true;
+                }
+            }
+        } catch (NoSuchElementException e) {
+            logger.error(e.getMessage());
+        }
+        return false;
+    }
+
+    private boolean isAxiomChangeToRemove(OntologyChange change, Set<OWLAxiom> axiomsAll) {
+        try {
+            OWLAxiom axiom = change.getAxiomOrThrow();
+            if (!axiom.getAnonymousIndividuals().isEmpty()) {
+                HashSet<OWLAxiom> sameAx = getEqualAxiomsExceptForBlankNodes(axiom, axiomsAll);
+                if (!sameAx.isEmpty()) {
+                    return true;
+                }
+            }
+        } catch (NoSuchElementException e) {
+            logger.error(e.getMessage());
+        }
+        return false;
+    }
+
+    private String removeBlankNodeIds(String st) {
+        return st.replaceAll("\\p{Zs}_:[a-zA-Z0-9]+", "")
+                .replaceAll("_:[a-zA-Z0-9]+\\p{Zs}", "")
+                .replaceAll("\\p{Zs}_:[a-zA-Z0-9]+\\p{Zs}", "");
+    }
+
+    private HashSet<OWLAnnotation> getEqualAnnotationsExceptForBlankNodes(OWLAnnotation annotation, Set<OWLAnnotation> annotationsAll) {
+        HashSet<OWLAnnotation> sameAn = new HashSet<>();
+        String annotationS = removeBlankNodeIds(annotation.toString());
+        for (OWLAnnotation ontAnnotation : annotationsAll) {
+            try {
+                if (!ontAnnotation.getAnonymousIndividuals().isEmpty()) {
+                    String ontAnnotationS = removeBlankNodeIds(annotation.toString());
+                    if (annotationS.equals(ontAnnotationS)) {
+                        sameAn.add(ontAnnotation);
+                    }
+                }
+            } catch (NoSuchElementException e) {
+                logger.error(e.getMessage());
+            }
+        }
+        return sameAn;
+    }
+
+    private HashSet<OWLAxiom> getEqualAxiomsExceptForBlankNodes(OWLAxiom axiom, Set<OWLAxiom> axiomsAll) {
+        HashSet<OWLAxiom> sameAx = new HashSet<>();
+        String axiomS = removeBlankNodeIds(axiom.toString());
+        for (OWLAxiom ontAxiom : axiomsAll) {
+            try {
+                if (!ontAxiom.getAnonymousIndividuals().isEmpty()) {
+                    String ontAxiomS = removeBlankNodeIds(ontAxiom.toString());
+                    if (axiomS.equals(ontAxiomS)) {
+                        sameAx.add(ontAxiom);
+                    }
+                }
+            } catch (NoSuchElementException e) {
+                logger.error(e.getMessage());
+            }
+        }
+        return sameAx;
+    }
 
     private OWLOntology getOntologyFromEndpoint(IRI projectEndpoint, String tboxGraph, OWLOntologyManager owlOntologyManager) {
 
